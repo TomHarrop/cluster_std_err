67a9e05fc3bacab0e09504bce674bbcb75919cc0
Tom Harrop <twharrop@gmail.com>
2023-03-23 15:06:12 +1100

    don't cluster zeros

--- test_script.py
+++ test_script.py
@@ -10,6 +10,7 @@ sorted_df = df_clustered.sort_values(by='cluster_id')
 sorted_df.to_csv('bedtools_closestbed_sorted.csv')
 
 # count and summarize the clusters
-cluster_summary = df_clustered.summarize_clusters()
+cluster_summary = df_clustered.get_cluster_summary()
+print(cluster_summary)
 cluster_summary.to_csv('bedtools_cluster_summary.csv')
 
